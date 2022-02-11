5e4790d728221f4443999743745e51382be8ca12
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-11 14:21:09 +0600

    added a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let example1 = document.querySelector('#example1');
 example1.addEventListener('click', function(event){
-  event.preventDefault()
+  event.preventDefault(); //кнопка перестанет работать
 
   //alert ('Hello world');
 });
