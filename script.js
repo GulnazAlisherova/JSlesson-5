75765faba0dc55746891af47be80184f314c01c1
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-11 14:47:10 +0600

    added position

--- script.js
+++ script.js
@@ -32,3 +32,4 @@ let example = document.createElement('section', );
 example.style.width = '20px';
 example.style.height = '20px';
 example.style.backgroundColor = 'black';
+example.style.position = 
