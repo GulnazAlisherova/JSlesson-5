2da217d0f3b86f62fcb7d54b5387557647adb93d
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-11 15:30:11 +0600

    added includes

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
-let example1 = document.querySelector('#example1');
-example1.addEventListener('click', function(event){
+// let example1 = document.querySelector('#example1');
+// example1.addEventListener('click', function(event){
   
-  window.location.href = "http://bing.com"; // window. без него тоже будет работать
-  event.preventDefault(); //кнопка перестанет работать
+//   window.location.href = "http://bing.com"; // window. без него тоже будет работать
+//   event.preventDefault(); //кнопка перестанет работать
   
-  //alert ('Hello world');
- });
-document.body.addEventListener('contextmenu', function(event){
-  event.preventDefault();
-});
+//   //alert ('Hello world');
+//  });
+// document.body.addEventListener('contextmenu', function(event){
+//   event.preventDefault();
+// });
 
-document.body.addEventListener('dbclick', function(event){
-  event.preventDefault();
-});
+// document.body.addEventListener('dbclick', function(event){
+//   event.preventDefault();
+// });
 
-document.querySelector('div').addEventListener('dbclick', function(event){
-  console.log(event);
-  event.preventDefault();
-});
+// document.querySelector('div').addEventListener('dbclick', function(event){
+//   console.log(event);
+//   event.preventDefault();
+// });
 
-document.querySelector('div').addEventListener('dbclick', function(event){
-  alert('Hello!');
-  //event.preventDefault(); --doesn't work
-});
+// document.querySelector('div').addEventListener('dbclick', function(event){
+//   alert('Hello!');
+//   //event.preventDefault(); --doesn't work
+// });
 
-document.querySelector('div').addEventListener('mousemove', function(event){
-  event.preventDefault();
-});
+// document.querySelector('div').addEventListener('mousemove', function(event){
+//   event.preventDefault();
+// });
 
-let example = document.createElement('section', );
-example.style.width = '20px';
-example.textContent = '👺';
-example.style.fontSize = '1rem';
-example.style.height = '20px';
-example.style.backgroundColor = 'black';
-example.style.position = 'absolute';
-document.body.append(example);
+// let example = document.createElement('section', );
+// example.style.width = '20px';
+// example.textContent = '👺';
+// example.style.fontSize = '1rem';
+// example.style.height = '20px';
+// example.style.backgroundColor = 'black';
+// example.style.position = 'absolute';
+// document.body.append(example);
 
-document.body.addEventListener('mousemove', function(event){
-  example.style.top = event.clientY + 'px';
-  example.style.left = event.clientX + 'px';
-   // console.log(event.clientX);
-   // console.log(event.clientY);
-});
+// document.body.addEventListener('mousemove', function(event){
+//   example.style.top = event.clientY + 'px';
+//   example.style.left = event.clientX + 'px';
+//    // console.log(event.clientX);
+//    // console.log(event.clientY);
+// });
 
 let example2 = document.querySelector('#example2');
 
 example2.addEventListener('keydown', function(event){
   console.log('keydown', event);
-  if (event.key); //
+  if (preventList.includes(event.key)) {
+    
+  } 
 });
+
