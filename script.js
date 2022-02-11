81e9687dc6580bfbe005cf1eda5858c3975fc5db
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-11 14:28:01 +0600

    added //comment

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let example1 = document.querySelector('#example1');
 example1.addEventListener('click', function(event){
-  // event.preventDefault(); //кнопка перестанет работать
-  window.location.href = "http://bing.com";
-
+  
+  window.location.href = "http://bing.com"; // window. без него тоже будет работать
+  event.preventDefault(); //кнопка перестанет работать
+  
   //alert ('Hello world');
  });
+ 
 // let example2 = document.querySelector('#example2');
 // example2.addEventListener('click', function(event){
 //   event.preventDefault();
