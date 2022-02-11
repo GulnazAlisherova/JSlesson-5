a97ec3b4ccd4e2a0c2eb8aded7b973654caced59
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-11 14:25:06 +0600

    added window.location

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let example1 = document.querySelector('#example1');
 example1.addEventListener('click', function(event){
-  event.preventDefault(); //кнопка перестанет работать
+  // event.preventDefault(); //кнопка перестанет работать
+  window.location
 
   //alert ('Hello world');
-});
-let example2 = document.querySelector('#example2');
-example2.addEventListener('click', function(event){
-  event.preventDefault();
-});
+ });
+// let example2 = document.querySelector('#example2');
+// example2.addEventListener('click', function(event){
+//   event.preventDefault();
+// });
